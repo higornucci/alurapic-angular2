3bc236d0dfbfcb8f2872ca2625335bd62b58ef31
unknown <higor.nucci@dominio.ufms.br>
2017-10-02 13:08:40 -0400

    Direcionamento com routes

---
+++ client/app/app.module.ts
@@ -0,0 +1,17 @@
+import 'rxjs/add/operator/map';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { FotoModule } from './foto/foto.module';
+import { PainelModule } from './painel/painel.module';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ListagemComponent } from './listagem/listagem.component';
+import { HttpModule } from '@angular/http'
+import { routing } from './app.routes';
+
+@NgModule({
+    imports: [BrowserModule, FotoModule, PainelModule, HttpModule, routing],
+    declarations: [AppComponent, CadastroComponent, ListagemComponent],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
